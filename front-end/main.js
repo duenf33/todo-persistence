f3a3d841a928c2d47f22f4d5d2eedee81d6ffd77
fernando duenas <fernando39615@aim.com>
2020-11-22 14:52:36 -0500

    first commit

--- front-end/main.js
+++ front-end/main.js
@@ -2,14 +2,14 @@ const readline = require('readline');
 const fs = require('fs');
 
 
-const PATH_TO_TODOS_FILE = __dirname + '/../back-end/todos.json';
+const PATH_TO_TODOS_FILE = __dirname + './../back-end/todos.json';
 let todos = [];
 const interface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-const displayMenu = function() {
+const displayMenu = () => {
   const menu = `
 Your options are:
 
@@ -25,14 +25,36 @@ Your options are:
   interface.question(menu, handleMenu);
 }
 
-const displayTodos = function() {
+fs.readFile('./../back-end/todos.json', 'utf8', (err, data) => {
+  if(err){
+    console.log(err);
+  }
+  const obj = JSON.parse(data);
+  todos = obj.todos;
+  displayTodos();
+})
+
+const saveTodos = (todo) => {
+  const obj = {todos: todos}
+  const data = JSON.stringify(obj, null, 2);
+  fs.writeFile('./../back-end/todos.json', data, 'utf8', (err) => {
+    if(err){
+      console.log(error);
+      return;
+    }
+    console.log('Changes have been saved.');
+    displayTodos();
+  })
+}
+
+const displayTodos = () => {
   console.log('\nHere are your current todos:\n')
   for (let i = 0; i < todos.length; i++) {
-    console.log(i + 1 + '. ' + todos[i].text + ' ' + (todo[i].isComplete ? '✅' : '✖'));
+    console.log(i + 1 + '. ' + todos[i].text + ' ' + (todos[i].isComplete ? '✅' : '✖'));
   }
 }
 
-const add = function(answer) {
+const add = answer => {
   const todo = {
     text: answer,
     priority: 2,
@@ -40,17 +62,18 @@ const add = function(answer) {
   }
 
   todos.unshift(todo);
+  saveTodos();
   displayTodos();
   displayMenu();
 }
 
-const remove = function(num) {
+const remove = num => {
   todos.splice(num - 1, 1);
   displayTodos();
   displayMenu();
 }
 
-const toggleComplete = function(num) {
+const toggleComplete = num => {
   const todo = todos[num - 1];
   if (todo.isComplete) {
     todo.isComplete = false;
@@ -63,7 +86,7 @@ const toggleComplete = function(num) {
 }
 
 // or, with a bang operator:
-const toggleCompleteAlt = function(num) {
+const toggleCompleteAlt = num => {
   const todo = todos[num - 1];
   todo.isComplete = !todo.isComplete;
 
@@ -71,7 +94,7 @@ const toggleCompleteAlt = function(num) {
   displayMenu();
 }
 
-const togglePriority = function(num) {
+const togglePriority = num => {
   const todo = todos[num - 1];
   if (todo.priority === 1) {
     todo.priority = 2;
@@ -83,7 +106,7 @@ const togglePriority = function(num) {
   displayMenu();
 }
 
-const removeCompletedTodos = function() {
+const removeCompletedTodos = () => {
   todos = todos.filter(function(todo) {
     return todo.isComplete === false;
   })
@@ -93,27 +116,27 @@ const removeCompletedTodos = function() {
 }
 
 // or with a nifty one-line arrow function
-const removeCompletedTodosAlt = function() {
+const removeCompletedTodosAlt = () => {
   todos = todos.filter((todo) => todo.isComplete === false);
 
   displayTodos();
   displayMenu();
 }
 
-const handleMenu = function(cmd) {
+const handleMenu = cmd => {
   if (cmd === '1') {
-    interface.question('\nWhat should go on your list? ', add)
+    interface.question('\nWhat should go on your list? ', add);
   } else if (cmd === '2') {
     displayTodos();
-    interface.question('\nPlease pick a todo to remove: ', remove)
+    interface.question('\nPlease pick a todo to remove: ', remove);
   } else if (cmd === '3') {
     removeCompletedTodos();
   } else if (cmd === '4') {
     displayTodos();
-    interface.question('\nPlease pick a todo to check complete or incomplete: ', toggleComplete)
+    interface.question('\nPlease pick a todo to check complete or incomplete: ', toggleComplete);
   } else if (cmd === '5') {
     displayTodos();
-    interface.question('\nPlease pick a todo to toggle its priority: ', togglePriority)
+    interface.question('\nPlease pick a todo to toggle its priority: ', togglePriority);
   } else {
     console.log('Quitting!');
     interface.close();
